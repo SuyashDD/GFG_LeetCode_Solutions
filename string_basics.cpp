fe02efa7ef3894cdbb5fd7e346deee086353a404
Suyash Dabhane <spdabhane_m19@ce.vjti.ac.in>
2022-06-28 16:31:18 +0530

    Create string_basics.cpp

---
+++ string_basics.cpp
@@ -0,0 +1,15 @@
+// transform single char to lowercase or uppercase
+
+string s = "ABCADdssf";
+for(auto c: s)
+{
+  c = tolower(c); // need to assign the result 
+  c = toupper(c);
+}
+
+
+// Transform whole string to lowercase or uppercase
+string s = "ABSDA";
+
+transform(s.begin() , s.end() , s.begin() , :: tolower);
+transform(s.begin() , s.end() , s.begin() , :: toupper);
