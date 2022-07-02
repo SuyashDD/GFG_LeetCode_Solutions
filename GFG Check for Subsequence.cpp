905f554b07ac2fda417bdce5b6b60c5a535e47a1
Suyash Dabhane <spdabhane_m19@ce.vjti.ac.in>
2022-07-02 16:26:35 +0530

    Update GFG Check for Subsequence.cpp

--- GFG Check for Subsequence.cpp
+++ GFG Check for Subsequence.cpp
@@ -39,3 +39,42 @@ bool isSubSequence(string p, string s)
         }
         return false;
     }
+
+// Optimized Function
+bool check(string &s1 , string &s2)
+{
+    int s1l = s1.length();
+    int s2l = s2.length();
+    if(s1l < s2l)
+        return false;
+    int j = 0 ;
+    for(int i = 0 ; i < s1l ; i++)
+    {
+        if(s1[i] == s2[j])
+        {
+            j++;
+        }
+    }
+    if(j == s2l)
+        return true;
+    return false;
+}
+
+
+// Recursive solution
+bool recursiveCheck(string s1 , string s2 , int m , int n)
+{
+    if(n == 0)
+        return true;
+    if(m == 0)
+        return false;
+    if(s1[m-1] == s2[n-1])
+    {
+        return recursiveCheck(s1 , s2 , m - 1 , n - 1);
+    }
+    else
+    {
+        return recursiveCheck(s1 , s2 , m-1 , n);
+    }
+    
+}
